6f361866fbdb448c3cbe284976181f22a36bc35c
simoncisf <72112354+simoncisf@users.noreply.github.com>
2021-08-22 21:33:39 -0400

    Update app.py

--- Mission-to-Mars/app.py
+++ Mission-to-Mars/app.py
@@ -22,7 +22,7 @@ def scrape():
    mars = mongo.db.mars
    mars_data = scraping.scrape_all()
    mars.update({}, mars_data, upsert=True)
-   return "Scraping Successful!"
+   return "scraping successful!"
 
 if __name__ == "__main__":
    app.run()
